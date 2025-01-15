0d6dc7f9af016150a177767e0f409cc7fe16600a
Meisyaa <meyziasabrinayunikaputri@gmail.com>
2025-01-15 17:42:12 +0700

    create recipe update feature

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -94,3 +94,73 @@ exports.deleteRecipe = async (req, res) => {
     res.status(500).send("Error: " + error.message);
   }
 };
+
+exports.editRecipe = async (req, res) => {
+  const { recipeId } = req.params;
+
+  try {
+    const recipeRef = ref(db, `recipes/${recipeId}`);
+    const recipeSnapshot = await get(recipeRef);
+
+    if (!recipeSnapshot.exists()) {
+      return res.status(404).send("Recipe not found.");
+    }
+
+    const recipeData = recipeSnapshot.val();
+
+    res.render("home/editRecipe", { recipeId, recipeData });
+  } catch (error) {
+    res.status(500).send("Error: " + error.message);
+  }
+};
+
+exports.updateRecipe = async (req, res) => {
+  const { recipeId } = req.params;
+  const { title, description, prep, cook } = req.body;
+
+  try {
+    const recipeRef = ref(db, `recipes/${recipeId}`);
+    const recipeSnapshot = await get(recipeRef);
+
+    if (!recipeSnapshot.exists()) {
+      return res.status(404).send("recipe not found.");
+    }
+
+    const recipeData = recipeSnapshot.val();
+
+    let recipeImagePath = recipeData.recipeImagePath;
+
+    if (req.file) {
+      const oldFilePath = recipeData.recipeImagePath.split("/").pop();
+      console.log(oldFilePath);
+      await supabase.storage.from("recipe-images").remove([oldFilePath]);
+
+      const uniqueFilename = `${Date.now()}_${req.file.originalname}`;
+      const buffer = req.file.buffer;
+
+      const { data, error } = await supabase.storage
+        .from("recipe-images")
+        .upload(uniqueFilename, buffer, {
+          contentType: req.file.mimetype,
+        });
+
+      if (error) {
+        return res.status(500).send({ error: error.message });
+      }
+
+      recipeImagePath = `${process.env.SUPABASE_URL}/storage/v1/object/public/${data.fullPath}`;
+    }
+
+    await set(recipeRef, {
+      title,
+      description,
+      prep,
+      cook,
+      recipeImagePath,
+    });
+
+    res.redirect("/home");
+  } catch (error) {
+    res.status(500).send("Error: " + error.message);
+  }
+};
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -10,5 +10,12 @@ router.get("/", homeController.getDashboard);
 router.get("/recipes", homeController.getRecipes);
 router.post("/upload-recipe", upload.single("file"), homeController.postRecipe);
 router.post("/delete-recipe/:recipeId", homeController.deleteRecipe);
+router.get("/edit-recipe/:recipeId", homeController.editRecipe);
+
+router.post(
+  "/update-recipe/:recipeId",
+  upload.single("file"),
+  homeController.updateRecipe
+);
 
 module.exports = router;
